b3f85964353c9388d76234ae5da55f8a2e17f13b
Jón Rúnar Helgason <dev@jonrh.is>
2020-04-15 18:21:34 +0100

    Added a list of links to the Verkefni and Þjónustur pages
    These changes are as of now mostly intended to serve as temporary page navigation. The links should be correct on the project page but most of the links on the service page are wrong and point to the Fosshótel project. To be fixed later.

--- src/pages/verkefni/index.js
+++ src/pages/verkefni/index.js
@@ -1,8 +1,29 @@
 import React from "react";
 
+import Layout from "../../components/Layout";
+
 const Verkefni = (props) => {
   return (
-    <p>Verkefni</p>
+    <Layout>
+      <h1>Verkefni</h1>
+      <ul>
+        <li>
+          <a href="/verkefni/fosshótel">Fosshótel</a>
+        </li>
+        <li>
+          <a href="/verkefni/lindex">Lindex</a>
+        </li>
+        <li>
+          <a href="/verkefni/sólvangur">Sólvangur Hjúkrunarheimili</a>
+        </li>
+        <li>
+          <a href="/verkefni/blikahöfði5-7">Blikahöfði 5 - 7</a>
+        </li>
+        <li>
+          <a href="/verkefni/vegghamrar2-10">Vegghamrar 2 - 10</a>
+        </li>
+      </ul>
+    </Layout>
   );
 };
 
--- src/pages/þjónustur/index.js
+++ src/pages/þjónustur/index.js
@@ -1,5 +1,30 @@
 import React from "react";
 
-const Þjónustur = () => <p>Þjónustur</p>;
+import Layout from "../../components/Layout";
+
+const Þjónustur = () => {
+  return (
+    <Layout>
+      <h1>Þjónustur</h1>
+      <ul>
+        <li>
+          <a href="/þjónustur/íbúðarhúsnæði">Íbúðarhúsnæði</a>
+        </li>
+        <li>
+          <a href="/verkefni/fosshótel">Fjölbýlishús</a>
+        </li>
+        <li>
+          <a href="/verkefni/fosshótel">Atvinnuhúsnæði</a>
+        </li>
+        <li>
+          <a href="/verkefni/fosshótel">Sérverkefni</a>
+        </li>
+        <li>
+          <a href="/verkefni/fosshótel">Innimálun</a>
+        </li>
+      </ul>
+    </Layout>
+  );
+};
 
 export default Þjónustur;
