b7b6da7da652184660ad9a0e329e9019ce1c5740
Jón Rúnar Helgason <dev@jonrh.is>
2020-06-13 14:39:22 +0100

    Added a placeholder page for Innimálun

---
+++ src/pages/málningarþjónusta/innimálun.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Layout from "../../components/Layout";
+
+const Innimálun = () => {
+  return (
+    <Layout>
+      <h1>Innimálun</h1>
+    </Layout>
+  );
+};
+
+export default Innimálun;
