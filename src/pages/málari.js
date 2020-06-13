f301724445277efe53456f1196b8b62d0b3684d0
Jón Rúnar Helgason <dev@jonrh.is>
2020-06-13 14:35:48 +0100

    Added placeholder pages for /málarameistari, /málari, /málningarþjónusta

---
+++ src/pages/málari.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Layout from "../components/Layout";
+
+const Málari = () => {
+  return (
+    <Layout>
+      <h1>Málari</h1>
+    </Layout>
+  );
+};
+
+export default Málari;
---
+++ src/pages/málningarþjónusta.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Layout from "../components/Layout";
+
+const Málningarþjónusta = () => {
+  return (
+    <Layout>
+      <h1>Málningarþjónusta</h1>
+    </Layout>
+  );
+};
+
+export default Málningarþjónusta;
