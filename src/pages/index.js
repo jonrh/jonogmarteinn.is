0d99c4c5c555d590a99d4a83bd87f699db08d268
Jón Rúnar Helgason <dev@jonrh.is>
2020-08-31 13:01:51 +0100

    Refactored Málarameistari text, automatic years of experience
    Before this change the text description for Málarameistari (on the frontpage and /málarameistari route) were duplicated. With this change the text is now defined in single place so if the text is updated it will stay in sync.
    
    In addition the years of experience is now automatically calculated so we don't have to update it every year. It's a crude calculation that gets incremented on a new year but it will suffice for now.

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,6 +15,7 @@ import Blikahofdi1 from "./../images/blikahofdi1.jpg";
 import Vegghamrar1 from "./../images/vegghamrar2-10/vegghamrar1.jpg";
 import Profile1_Jon from "./../images/profile1_jon.jpg";
 import Profile2_Marteinn from "./../images/profile2_marteinn.jpg";
+import { MálarameistariTextDescription } from "./málarameistari";
 
 /** Convenience function to return a React style object with a background image */
 const backgroundImage = (image) => ({ backgroundImage: `url('${image}')` });
@@ -237,18 +238,7 @@ const Index = (props) => {
           <h2 className="h1 mb-5">Málarameistari</h2>
           <div className="row mb-5 text-justify">
             <div className="col-11 col-md-10 col-lg-8 mx-auto">
-              <p className="lead">
-                Jón Hafdal er <strong>löggiltur málarameistari</strong> og hefur að baki 20 ára
-                reynslu af málun, Marteinn 22 ár. Saman stofnuðu þeir fyrirtækið Jón og Marteinn
-                Málningarþjónusta ehf.
-              </p>
-
-              <p className="lead">
-                Fyrirtækið tekur að sér öll almenn málningarverkefni. Verkefni stór sem smá,
-                sérsniðið að óskum og þörfum og hvar sem er á landinu. Höfum einnig tekið að okkur
-                verkefni erlendis. Hjá fyrirtækinu starfa samtals 10 starfsmenn meðtalið Jóni og
-                Marteini.
-              </p>
+              <MálarameistariTextDescription />
             </div>
           </div>
           <div className="row justify-content-center">
--- src/pages/málarameistari.js
+++ src/pages/málarameistari.js
@@ -30,6 +30,27 @@ const Owner = ({name, image, phoneNumber}) => {
   );
 };
 
+export const MálarameistariTextDescription = () => {
+  const jonYearsOfExperience = new Date().getUTCFullYear() - 2000;
+  const marteinnYearsOfExperience = new Date().getUTCFullYear() - 1998;
+
+  return (
+    <>
+      <p className="lead">
+        Jón Hafdal er <strong>löggiltur málarameistari</strong> og hefur að baki{" "}
+        {jonYearsOfExperience} ára reynslu af málun, Marteinn {marteinnYearsOfExperience} ár.
+        Saman stofnuðu þeir fyrirtækið Jón og Marteinn Málningarþjónusta ehf.
+      </p>
+
+      <p className="lead">
+        Fyrirtækið tekur að sér öll almenn málningarverkefni. Verkefni stór sem smá, sérsniðið að
+        óskum og þörfum og hvar sem er á landinu. Höfum einnig tekið að okkur verkefni erlendis.
+        Hjá fyrirtækinu starfa samtals 10 starfsmenn meðtalið Jóni og Marteini.
+      </p>
+    </>
+  );
+}
+
 const Málarameistari = () => {
   return (
     <Layout>
@@ -48,18 +69,7 @@ const Málarameistari = () => {
 
         <Row className="pt-5">
           <Col lg={8} md={10} sm={11} xs={11} className="text-justify mx-auto">
-            <p className="lead">
-              Jón Hafdal er <strong>löggiltur málarameistari</strong> og hefur að baki 20 ára
-              reynslu af málun, Marteinn 22 ár. Saman stofnuðu þeir fyrirtækið Jón og Marteinn
-              Málningarþjónusta ehf.
-            </p>
-
-            <p className="lead">
-              Fyrirtækið tekur að sér öll almenn málningarverkefni. Verkefni stór sem smá,
-              sérsniðið að óskum og þörfum og hvar sem er á landinu. Höfum einnig tekið að okkur
-              verkefni erlendis. Hjá fyrirtækinu starfa samtals 10 starfsmenn meðtalið Jóni og
-              Marteini.
-            </p>
+            <MálarameistariTextDescription />
 
             <img
               src={certificate}
