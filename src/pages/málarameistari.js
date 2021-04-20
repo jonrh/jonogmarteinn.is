913f2726372efd4b8f587c3c1eff8ed7a69d0dcf
Jón Rúnar Helgason <dev@jonrh.is>
2021-04-20 19:13:47 +0000

    Minor improvement of SEO word choices

--- src/pages/málarameistari.js
+++ src/pages/málarameistari.js
@@ -31,14 +31,15 @@ export const MálarameistariTextDescription = () => {
     <>
       <p className="lead">
         Jón Hafdal er <strong>löggiltur málarameistari</strong> og hefur að baki{" "}
-        {jonYearsOfExperience} ára reynslu af málun, Marteinn {marteinnYearsOfExperience} ár.
-        Saman stofnuðu þeir fyrirtækið Jón og Marteinn Málningarþjónusta ehf.
+        {jonYearsOfExperience} ára reynslu af málun. Marteinn hefur starfað sem málari í{" "}
+        {marteinnYearsOfExperience} ár. Saman stofnuðu þeir fyrirtækið Jón og Marteinn
+        Málningarþjónusta ehf.
       </p>
 
       <p className="lead">
-        Fyrirtækið tekur að sér öll almenn málningarverkefni. Verkefni stór sem smá, sérsniðið að
-        óskum og þörfum og hvar sem er á landinu. Höfum einnig tekið að okkur verkefni erlendis.
-        Hjá fyrirtækinu starfa samtals 10 starfsmenn meðtalið Jóni og Marteini.
+        Fyrirtækið tekur að sér öll almenn verk þar sem þarf að mála. Verkefni stór sem smá,
+        sérsniðið að óskum og þörfum og hvar sem er á landinu. Höfum einnig tekið að okkur verkefni
+        erlendis. Hjá fyrirtækinu starfa samtals 10 málarar meðtalið Jóni og Marteini.
       </p>
     </>
   );
